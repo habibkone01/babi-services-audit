4976c8130cf2c7b174227dd214f2d459b66168ff
Aloneeveryday2 <axelteky333@gmail.com>
2026-06-15 19:52:40 +0000

    feat :  Login & resgister request

--- babi/routes/api.php
+++ babi/routes/api.php
@@ -5,8 +5,17 @@ use App\Http\Controllers\Api\CategorieController;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\ReservationController;
 use App\Http\Controllers\Api\AdminDashboardController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+});
 
 Route::apiResource('prestataires', PrestaireController::class);
 Route::apiResource('categories', CategorieController::class);
