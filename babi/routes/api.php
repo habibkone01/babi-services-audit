a8f4b0e92b46fbf53e6fcc0fc53427aba8e551cb
habibkone0170 <habibkone258@gmail.com>
2026-07-17 23:47:42 +0000

    A07: Rate limiting sur /login (throttle 3 tentatives/minute/IP)

--- babi/routes/api.php
+++ babi/routes/api.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login',    [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->middleware('throttle:3,1');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
